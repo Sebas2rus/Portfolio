c2f3b1680cf3a1d45eb6a855c735e525e9670a3c
Sebas1rus <sebastian.lopez@parqueexplora.org>
2023-05-26 21:46:07 -0500

    update for mobile

--- app/components/layout/Navbar.jsx
+++ app/components/layout/Navbar.jsx
@@ -1,41 +1,97 @@
 /* eslint-disable jsx-a11y/anchor-is-valid */
 import Link from 'next/link';
-import React from 'react';
+import React, { useState } from 'react';
 import styles from './styles.module.scss';
 
 export default function Navbar() {
+  const [display, setDisplay] = useState('noVisible');
+
   return (
-    <header className={styles.header}>
-      <Link href="#" className={styles.headerLogo}>
-        Logo
-      </Link>
-      <ul className={styles.headerList}>
-        <li>
-          <Link href="#" className={styles.headerListLink}>
-            <span>01.</span>About
-          </Link>
-        </li>
-        <li>
-          <Link href="#" className={styles.headerListLink}>
-            <span>02.</span>Experience
-          </Link>
-        </li>
-        <li>
-          <Link href="#" className={styles.headerListLink}>
-            <span>03.</span>Work
-          </Link>
-        </li>
-        <li>
-          <Link href="#" className={styles.headerListLink}>
-            <span>04.</span>Contact
-          </Link>
-        </li>
-        <li>
-          <button href="#" type="button" className="btn-outline-green a">
-            Resume
+    <div>
+      <header className={styles.header}>
+        <Link href="#" className={styles.headerLogo}>
+          Logo
+        </Link>
+        <ul className={styles.headerList}>
+          <li>
+            <Link href="#" className={styles.headerListLink}>
+              <span>01.</span>About
+            </Link>
+          </li>
+          <li>
+            <Link href="#" className={styles.headerListLink}>
+              <span>02.</span>Experience
+            </Link>
+          </li>
+          <li>
+            <Link href="#" className={styles.headerListLink}>
+              <span>03.</span>Work
+            </Link>
+          </li>
+          <li>
+            <Link href="#" className={styles.headerListLink}>
+              <span>04.</span>Contact
+            </Link>
+          </li>
+          <li>
+            <button href="#" type="button" className="btn-outline-green a">
+              Resume
+            </button>
+          </li>
+        </ul>
+      </header>
+      <header className={styles.mobile}>
+        <div className={styles.mobileHeader}>
+          <a href="#">Logo</a>
+          <button
+            type="button"
+            onClick={() => setDisplay('visible')}
+            className="btn-menu"
+          >
+            <i className="bi bi-list" />
           </button>
-        </li>
-      </ul>
-    </header>
+        </div>
+        <div
+          role="button"
+          className={`${
+            display === 'noVisible' ? styles.invisible : styles.mobileNavbar
+          } ? `}
+          onClick={() => setDisplay('noVisible')}
+        >
+          <ul className={styles.mobileNavbarList}>
+            <li>
+              <Link href="#" className={styles.mobileNavbarListLink}>
+                <span>01.</span> <br />
+                About
+              </Link>
+            </li>
+            <li>
+              <Link href="#" className={styles.headerListLink}>
+                <span>02.</span> <br />
+                Experience
+              </Link>
+            </li>
+            <li>
+              <Link href="#" className={styles.headerListLink}>
+                <span>03.</span> <br />
+                Work
+              </Link>
+            </li>
+            <li>
+              <Link href="#" className={styles.headerListLink}>
+                <span>04.</span>
+                <br />
+                Contact
+              </Link>
+            </li>
+            <li>
+              <button href="#" type="button" className="btn-outline-green a">
+                Resume
+              </button>
+            </li>
+          </ul>
+        </div>
+      </header>
+    </div>
   );
 }
